86fb8e4c185237f420425afbee1f8c4e3a64c71e
github username <github email address>
2020-03-30 13:09:38 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+
+
+import React, { Component } from 'react'
+
+class EyesOnMe extends Component {
+
+  logFocus = () => {
+    console.log("Good!")
+  }
+  logBlur = () => {
+    console.log("Hey! Eyes on me!")
+  }
+
+
+  render() {
+    return (
+      <button onFocus={this.logFocus} onBlur={this.logBlur} >FOCUS!</button>
+    )
+  }
+
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+
+
+import React, { Component } from 'react'
+
+class Keypad extends Component {
+
+  logger = () => {
+    console.log('Entering password...')
+}
+
+  render() {
+    return (
+      <input onKeyUp={this.logger} type="password" />
+    )
+  }
+
+}
+
+export default Keypad
