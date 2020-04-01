395cefefcf17fa97a142cd67946edd742df415e7
adit313 <app2116@gmail.com>
2020-04-01 19:41:05 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,35 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  focusHandler = (event) => {
+      console.log('Good!')
+  }
+
+  blurHandler = (event) => {
+    console.log('Hey! Eyes on me!')
+}
+
+  render() {
+    return (
+      <div>
+          <button type="button"
+          onFocus={this.focusHandler}
+          onBlur={this.blurHandler}>>
+          Click Me!
+          </button>
+      </div>
+    );
+  }
+}
+EyesOnMe.propTypes = {
+
+};
+EyesOnMe.defaultProps = {
+
+};
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,27 @@
 // Code Keypad Component Here
+import React from 'react';
+
+export default class Keypad extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  
+keyUpFunction = (event) => {
+    console.log('Entering password...')
+}
+
+render() {
+  return (
+    <div><input onKeyUp={this.keyUpFunction} type="password" /></div>
+  );
+}
+}
+
+Keypad.propTypes = {
+
+};
+Keypad.defaultProps = {
+
+};
+
